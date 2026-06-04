1551d58c2939a3dca812c31e426893e8186f9c89
sylnox <maximepic256@gmail.com>
2026-06-04 13:51:53 +0200

    Deploy commands in index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
+const deployCommands = require('./deploy-commands')
+
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
